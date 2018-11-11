300d06a32ebbf6fecb1deb8a551f9c5efa509c99
kdev29 <leoc_2911@outlook.com>
2018-11-10 20:52:12 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,25 @@
 
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-
+  './logo_italika1.png',
+  './img/logo_italika.png',
+  './index.html',
+  './css/estilos_generales_adm.css',
+  './css/reset.css',
+  './css/menurefacciones.css',
+  './css/mano.css',  
+  './css/tablerodeconciliacion.css',  
+  './css/jquery-ui.css',  
+  './css/modalbox.css',  
+  './css/datatables.min.css',  
+  './css/font-roboto.css',  
+  './css/font-awesome.css',  
+  './js/menu.js', 
+  './js/datatables.min.js' ,
+  './js/jquery-ui.js' ,
+  './js/jquery-ventana2.js' ,
+  './js/jquery.min.js',
+  './img/italika_bandera_mexico.png'
 ];
 
 self.addEventListener('install', function(event){
